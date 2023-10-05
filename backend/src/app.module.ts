aa9e7a8cd345a00a3986eca87387c375436a5ba0
dLogan807 <dylanlogan316@gmail.com>
2023-10-06 10:12:18 +1300

    add auto line-end config to Prettier

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Article, ArticleSchema } from './articles/article.schema';
 import { ArticleModule } from './articles/article.module';
 
 dotenv.config();
-console.log("My connection");
+console.log('My connection');
 console.log(process.env.CONNECTION_STRING);
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb+srv://${process.env.CONNECTION_STRING}/articles`),
-    MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema}]),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.CONNECTION_STRING}/articles`,
+    ),
+    MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }]),
     ArticleModule,
-  ]
+  ],
 })
 export class AppModule {}
