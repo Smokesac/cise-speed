5f3f5cde209cc875e8869caa40768a00023fca58
dLogan807 <dylanlogan316@gmail.com>
2023-10-07 16:20:09 +1300

    configure routes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,18 @@ import { Article, ArticleSchema } from './articles/article.schema';
 import { ArticleModule } from './articles/article.module';
 import { ModArticle, ModArticleSchema } from './modArticles/modArticle.schema';
 import { ModArticleModule } from './modArticles/modArticle.module';
+import { AnalystArticleModule } from './analystArticles/analystArticle.module';
+import { RejectedArticleModule } from './rejectedArticles/rejectedArticle.module';
+import { TagModule } from './tags/tag.module';
+import {
+  AnalystArticle,
+  AnalystArticleSchema,
+} from './analystArticles/analystArticle.schema';
+import {
+  RejectedArticle,
+  RejectedArticleSchema,
+} from './rejectedArticles/rejectedArticle.schema';
+import { Tag, TagSchema } from './tags/tag.schema';
 
 dotenv.config();
 console.log('My connection');
@@ -17,9 +29,15 @@ console.log(process.env.CONNECTION_STRING);
     MongooseModule.forFeature([
       { name: Article.name, schema: ArticleSchema },
       { name: ModArticle.name, schema: ModArticleSchema },
+      { name: AnalystArticle.name, schema: AnalystArticleSchema },
+      { name: RejectedArticle.name, schema: RejectedArticleSchema },
+      { name: Tag.name, schema: TagSchema },
     ]),
     ArticleModule,
     ModArticleModule,
+    AnalystArticleModule,
+    RejectedArticleModule,
+    TagModule,
   ],
 })
 export class AppModule {}
