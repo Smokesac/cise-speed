56621413bf3a57f4c327af091a8b4a2f36e7765e
dLogan807 <dylanlogan316@gmail.com>
2023-10-07 17:42:53 +1300

    add NextUI dependency

--- frontend/src/pages/admin.tsx
+++ frontend/src/pages/admin.tsx
@@ -1,15 +1,32 @@
 import { GetStaticProps, NextPage } from "next";
 import SortableTable from "../components/table/SortableTable";
 import data from "../utils/dummydata.json";
+
+import {
+    Table,
+    TableHeader,
+    TableBody,
+    TableColumn,
+    TableRow,
+    TableCell
+  } from "@nextui-org/table";
+
 interface ArticlesInterface {
     id: string;
     title: string;
-    authors: string;
-    source: string;
-    pubyear: string;
-    doi: string;
+    authors: string[];
+    journal: string;
+    publicationYear: number;
+    volume: string;
+    numberPages: string;
+    DOI: string;
+    sEPractice: string;
     claim: string;
-    evidence: string;
+    researchType: string;
+    participantType: string;
+    evidenceResult: string;
+    tags: string[];
+    summary: string;
 }
 
 type ArticlesProps = {
