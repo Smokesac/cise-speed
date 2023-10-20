a66600299426182cacfe599e106d8531fa8791b5
dLogan807 <dylanlogan316@gmail.com>
2023-10-20 17:08:23 +1300

    add cors configurations

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const cors = require('cors');
-  app.use(cors());
+  //app.use(cors());
+  app.use(
+    cors({
+      origin: ['https://cise-speed-frontend.vercel.app'],
+      methods: ['POST', 'GET', 'PUT', 'DELETE'],
+    }),
+  );
   app.enableCors();
   await app.listen(5000);
 }
