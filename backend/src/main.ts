0b58e84053611e5c23501e8309cd8d64cf99066d
dLogan807 <dylanlogan316@gmail.com>
2023-10-22 16:25:16 +1300

    move cors to app.js

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,15 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const cors = require('cors');
-  app.use(cors(
-    { 
-        origin: ["https://cise-speed-frontend.vercel.app"],
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], 
-        credentials: true 
-    }
-));
-  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
