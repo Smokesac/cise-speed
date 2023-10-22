bbd43c98f04f33619caf83e310787c6d653485a2
dLogan807 <dylanlogan316@gmail.com>
2023-10-22 15:11:04 +1300

    update app.js to use cors

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,15 @@ connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
+// cors
+app.use(cors(
+    { 
+        origin: ["https://cise-speed-frontend.vercel.app"],
+        methods: ["GET","OPTIONS","PATCH","DELETE","POST","PUT"], 
+        credentials: true 
+    }
+));
+
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
