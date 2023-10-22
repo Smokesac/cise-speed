ef00349dd660c97dcc22db53f003dcdedd0178d1
dLogan807 <dylanlogan316@gmail.com>
2023-10-22 17:20:48 +1300

    set cors options in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,13 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const cors = require('cors');
-  app.use(
-    cors({
-      origin: ['*'],
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    }),
-  );
-  app.enableCors();
+  app.use(cors());
+  app.enableCors({
+    origin: ['*'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  });
   await app.listen(5000);
 }
 bootstrap();
