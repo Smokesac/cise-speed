a6ab36a1ec2b78730686bba6b24fa32eddc338c9
dLogan807 <dylanlogan316@gmail.com>
2023-10-22 15:21:15 +1300

    revert changes to app.js and retry.json

--- backend/app.js
+++ backend/app.js
@@ -8,15 +8,6 @@ connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
-// cors
-app.use(cors(
-    { 
-        origin: ["*"],
-        methods: ["GET","DELETE","POST","PUT"], 
-        credentials: true 
-    }
-));
-
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
