7eedee07f6f73af3f241b1d255b367c06e543ab5
dLogan807 <dylanlogan316@gmail.com>
2023-10-22 17:03:14 +1300

    move cors to index.ts

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require('cors');
 
 const app = express();
 
 // Connect Database
 connectDB();
-
-app.use(
-  cors({
-    origin: ['*'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  }),
-);
-
-app.enableCors();
 
 app.get('/', (req, res) => res.send('SPEED Backend'));
 
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const cors = require('cors');
+  app.use(
+    cors({
+      origin: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    }),
+  );
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
